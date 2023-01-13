de17a78d7761c624e58b13010964e67745322de0
julia-alessandra <julialessandra3@gmail.com>
2023-01-13 10:57:18 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const bombo = new Audio("sons/bombo.mp3")
 
-let tambor = document.querySelector("#circulo")
+let tambor = document.querySelector("#bombo")
 
 tambor.addEventListener("click", function(){
     bombo.play()
+})
+
+let play = document.querySelector("#play")
+let imagem = document.querySelector("#imgDaisy")
+const honeycomb = new Audio("sons/HONEYCOMB.mp3")
+play.addEventListener("click", function(){
+    honeycomb.play()
+    imagem.style.animation = 'girando 15s linear infinite alternate'
 })
